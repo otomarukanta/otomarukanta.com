89147e340b7fc9a89f9cc6e34eb92f13607dd915
otomarukanta <kanta208@gmail.com>
2015-06-08 22:44:33 +0900

    Add ECT as template engine

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var ect     = require('ect');
+var ectRenderer = ect({ watch: true, root: __dirname + '/views', ext: '.ect'});
+
 var app = express();
 
+app.set('view engine', 'ect');
+app.engine('ect', ectRenderer.render);
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index', {title: "test"});
 });
 
 app.listen(3000);
+console.log('Listening on port 3000');
